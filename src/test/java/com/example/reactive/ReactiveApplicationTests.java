0c181f23f4f7a9b286ff556cf34943cf3f4c1117
home <jasuil@daum.net>
2022-05-01 00:02:54 +0900

    change assert criteria position

--- src/test/java/com/example/reactive/ReactiveApplicationTests.java
+++ src/test/java/com/example/reactive/ReactiveApplicationTests.java
@@ -1,38 +1,29 @@
 package com.example.reactive;
 
 import com.example.reactive.dto.News;
-import com.example.reactive.repository.NewsRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.util.Assert;
 import reactor.core.publisher.Flux;
-import reactor.test.StepVerifier;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class ReactiveApplicationTests {
-	@Autowired
-	private ReactiveRedisOperations<String, News> newsOps;
+    @Autowired
+    private ReactiveRedisOperations<String, News> newsOps;
 
-	@Test
-	public void redisTest(){
-		News news1 = new News("결국 그런 거였어", Timestamp.valueOf(LocalDateTime.of(2022,4,28,23,50)));
-		News news2 = new News("대단해요", Timestamp.valueOf(LocalDateTime.of(2022,4,29,0,5)));
-		Flux.just(new Object[]{"1", news1},
-				new Object[] {"2", news2})
-				.flatMap(value -> newsOps.opsForValue().set((String) value[0], (News) value[1])).subscribe();
+    @Test
+    public void redisTest() {
+        News news1 = new News("결국 그런 거였어", Timestamp.valueOf(LocalDateTime.of(2022, 4, 28, 23, 50)));
+        News news2 = new News("대단해요", Timestamp.valueOf(LocalDateTime.of(2022, 4, 29, 0, 5)));
+        Flux.just(new Object[]{"1", news1},
+                        new Object[]{"2", news2})
+                .flatMap(value -> newsOps.opsForValue().set((String) value[0], (News) value[1])).subscribe();
 
-		News news3 = newsOps.opsForValue().get("1").block();
-		Assert.isTrue(news1.getContent().equals(news3.getContent()), "안돼에에~~");
-	}
+        newsOps.opsForValue().get("1").subscribe(n -> Assert.isTrue(news1.getContent().equals(n.getContent()), "안돼에에~~"));
+    }
 }
